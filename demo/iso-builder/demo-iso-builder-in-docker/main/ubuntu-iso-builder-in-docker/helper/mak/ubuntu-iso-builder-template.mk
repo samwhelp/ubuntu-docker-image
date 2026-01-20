c3b3ecb9d45c21d47197e41acde4223f6c2dde74
samwhelp <samwhelp@users.noreply.github.com>
2026-01-20 23:11:46 +0800

    fix

--- demo/iso-builder/demo-iso-builder-in-docker/main/ubuntu-iso-builder-in-docker/helper/mak/ubuntu-iso-builder-template.mk
+++ demo/iso-builder/demo-iso-builder-in-docker/main/ubuntu-iso-builder-in-docker/helper/mak/ubuntu-iso-builder-template.mk
@@ -1,5 +1,5 @@
 
 
 ubuntu-iso-builder-template:
-	$(REF_PLAN_DIR_PATH)/build.sh "ubuntu-iso-builder-template"
+	$(REF_PLAN_DIR_PATH)/master-build.sh "ubuntu-iso-builder-template"
 .PHONY: ubuntu-iso-builder-template
