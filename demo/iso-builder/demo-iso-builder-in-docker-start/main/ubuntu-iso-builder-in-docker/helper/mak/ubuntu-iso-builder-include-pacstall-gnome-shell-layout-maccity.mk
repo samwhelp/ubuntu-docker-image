d21b26edea24354a11af636fe5cffe8a45f13cdb
samwhelp <samwhelp@users.noreply.github.com>
2026-03-14 04:16:56 +0800

    fix

---
+++ demo/iso-builder/demo-iso-builder-in-docker-start/main/ubuntu-iso-builder-in-docker/helper/mak/ubuntu-iso-builder-include-pacstall-gnome-shell-layout-maccity.mk
@@ -0,0 +1,5 @@
+
+
+ubuntu-iso-builder-include-pacstall-gnome-shell-layout-maccity:
+	$(REF_PLAN_DIR_PATH)/master-build.sh "ubuntu-iso-builder-include-pacstall-gnome-shell-layout-maccity"
+.PHONY: ubuntu-iso-builder-include-pacstall-gnome-shell-layout-maccity
